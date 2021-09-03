03e3326563c934b5b6ff20028fd468fc5bf6efeb
LoTwT <709937065@qq.com>
2021-09-03 20:35:11 +0800

    chore(vite.config.ts): function type config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite"
+import { ConfigEnv, UserConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import path from "path"
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+  }
+}
