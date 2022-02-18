2fafd2d80c7f2d559047d283e3be0ed4a45d6528
LoTwT <709937065@qq.com>
2022-02-18 20:49:58 +0800

    feat: use template created by create-vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,57 +1,60 @@
+import { fileURLToPath, URL } from "url"
+
 import { defineConfig, loadEnv } from "vite"
-import vue from "@vitejs/plugin-vue"
 import ViteComponents from "unplugin-vue-components/vite"
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"
 import AutoImport from "unplugin-auto-import/vite"
-import path from "path"
+import vue from "@vitejs/plugin-vue"
 
+// https://vitejs.dev/config/
 export default defineConfig((config) => {
   const { mode } = config
 
   // .env
   const root = process.cwd()
+
   const { VITE_SERVER_PORT, VITE_SERVER_PROXY, VITE_SERVER_PROXY_TARGET } =
     loadEnv(mode, root) as unknown as ImportMetaEnv
-
-  const hasProxy = VITE_SERVER_PROXY && VITE_SERVER_PROXY_TARGET
+  const hasProxy = !!(VITE_SERVER_PROXY && VITE_SERVER_PROXY_TARGET)
 
   return {
     plugins: [
       vue(),
       ViteComponents({
         resolvers: [AntDesignVueResolver()],
-        dts: "./src/types/global/component.d.ts",
-        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        dts: "./AutoImport.d.ts",
+        include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
       }),
       AutoImport({
-        dts: "./src/types/global/auto-imports.d.ts",
-        imports: ["vue", "vue-router", "pinia"],
+        imports: ["vue", "vue-router", "pinia", "@vueuse/core"],
+        dts: "./AutoImport.d.ts",
         include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
       }),
     ],
 
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
 
     server: {
       port: VITE_SERVER_PORT || 3000,
-      // 此处默认转换 /api, 未做通用处理...
-      proxy: hasProxy && {
-        [VITE_SERVER_PROXY]: {
-          target: VITE_SERVER_PROXY_TARGET,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(VITE_SERVER_PROXY, ""),
-        },
-      },
+      proxy: hasProxy
+        ? {
+            [VITE_SERVER_PROXY]: {
+              target: VITE_SERVER_PROXY_TARGET,
+              changeOrigin: true,
+              rewrite: (path) => path.replace(VITE_SERVER_PROXY, ""),
+            },
+          }
+        : undefined,
     },
   }
 })
 
 // 环境变量
-// 如果需要在 src 内使用，请同时更新 src/env.d.ts
+// 如果需要在 src 内使用，请同时更新 env.d.ts
 // https://cn.vitejs.dev/guide/env-and-mode.html#intellisense
 interface ImportMetaEnv {
   readonly VITE_SERVER_PORT: number
