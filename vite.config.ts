5f6d9bd469d8424579e1a0828954acf25e2ab322
LoTwT <709937065@qq.com>
2022-02-19 22:57:38 +0800

    fix(vite.config.ts): remove base

--- vite.config.ts
+++ vite.config.ts
@@ -18,8 +18,6 @@ export default defineConfig((config) => {
   const hasProxy = !!(VITE_SERVER_PROXY && VITE_SERVER_PROXY_TARGET)
 
   return {
-    base: "/todolist/",
-
     plugins: [
       vue(),
       ViteComponents({
